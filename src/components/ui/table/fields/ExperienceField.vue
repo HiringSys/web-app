d751e1645e4d3e75f84b8a297c834e86467582c6
meguito <doasilva2009@hotmail.com>
2026-08-22 11:30:22 -0300

    chore: commit vuedraggable dependency and its supporting files

---
+++ src/components/ui/table/fields/ExperienceField.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+defineProps<{
+  years: number
+}>()
+
+</script>
+
+<template>
+  <span class="text-body font-medium text-black">{{ years }} {{ years === 1 ? 'ano' : 'anos' }}</span>
+</template>
