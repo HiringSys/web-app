1f62e8d43cd70d171fa003b00280ec2f3379cd85
meguito <doasilva2009@hotmail.com>
2026-08-24 07:34:13 -0300

    feat: implementing profile

--- src/components/ui/table/fields/CandidateField.vue
+++ src/components/ui/table/fields/CandidateField.vue
@@ -6,12 +6,14 @@ defineProps<{
   avatarUrl?: string
 }>()
 
+const mock = "src/service/mocks/user.jpg";
+
 </script>
 
 <template>
   <div class="flex items-center gap-2">
     <div class="h-10 w-10 aspect-square shrink-0 overflow-hidden rounded-full bg-gray-300">
-      <img v-if="avatarUrl" :src="avatarUrl" :alt="name" class="h-full w-full object-cover" />
+      <img :src="avatarUrl ? mock : mock" :alt="name" class="h-full w-full object-cover" />
     </div>
     <div class="flex flex-col gap-px max-w-slot-lg">
       <p class="truncate leading-none text-[1.05rem] font-semibold text-black pb-px">{{ name }}</p>
